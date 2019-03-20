a2c40c6c47c831388c7a0c7bfe5300060133f3af
Nobuhiro Takahashi <nt@feb19.jp>
2019-03-20 15:53:40 +0900

    Initial Commit

---
+++ HealthRecordsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthRecordsSample
+//
+//  Created by Nobuhiro Takahashi on 2019/03/20.
+//  Copyright © 2019年 Nobuhiro Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
